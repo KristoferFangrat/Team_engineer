30b679dc7a8837e0adfdd26bfd0abc3a476066ae
Jasmin <SwedishSolidSnake@gmail.com>
2024-09-11 11:39:01 +0200

    small changes to dim_empoyer (typos and consistent casing in 'Not Available'. Added testing for dim_job_ads. Added a sql script in a separate file to remove duplicates from staging.

--- dbt/dbt_code/models/dim/dim_employer.sql
+++ dbt/dbt_code/models/dim/dim_employer.sql
@@ -1,13 +1,13 @@
 with src_employer as (select * from {{ ref('src_employer') }})
 
 select {{dbt_utils.generate_surrogate_key(['id','employer_name'])}} as employer_id,
-{{ capitalize_first_letter_each_word("coalesce(employer_name, 'not available')") }} AS employer__name,
-coalesce(employer_organization_number, 'not available') AS employer_organization_number,
-{{ capitalize_first_letter_each_word("coalesce(workplace_street_address, 'not available')") }} as workplace_street_address,
-{{ capitalize_first_letter_each_word("coalesce(workplace_region, 'not available')") }} as workplace_region,
-{{ capitalize_first_letter_each_word("coalesce(workplace_postcode, 'not available')") }} as workplace_postcode,
-{{ capitalize_first_letter_each_word("coalesce(workplace_city, 'not available')") }} AS workplace_city,
+{{ capitalize_first_letter_each_word("coalesce(employer_name, 'Not Available')") }} AS employer_name,
+coalesce(employer_organization_number, 'Not Available') AS employer_organization_number,
+{{ capitalize_first_letter_each_word("coalesce(workplace_street_address, 'Not Available')") }} as workplace_street_address,
+{{ capitalize_first_letter_each_word("coalesce(workplace_region, 'Not Available')") }} as workplace_region,
+{{ capitalize_first_letter_each_word("coalesce(workplace_postcode, 'Not Available')") }} as workplace_postcode,
+{{ capitalize_first_letter_each_word("coalesce(workplace_city, 'Not Available')") }} AS workplace_city,
 workplace_country,
-{{ capitalize_first_letter_each_word("coalesce(workplace_municipality, 'not available')") }}workplace_municipality
+{{ capitalize_first_letter_each_word("coalesce(workplace_municipality, 'Not Available')") }}workplace_municipality
 
 from src_employer
