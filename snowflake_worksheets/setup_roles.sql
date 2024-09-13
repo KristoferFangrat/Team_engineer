44fdc82e71a338e9dfd30a91754db3e4372198f8
Jasmin <SwedishSolidSnake@gmail.com>
2024-09-13 11:28:37 +0200

    fixes

--- snowflake_worksheets/setup_roles.sql
+++ snowflake_worksheets/setup_roles.sql
@@ -1,5 +1,7 @@
 GRANT ROLE ACCOUNTADMIN TO USER ricky_user;
 GRANT ROLE T_E_streamlit_role TO USER ricky_user;
+GRANT ROLE T_E_streamlit_role TO USER rajneet_user;
+GRANT ROLE T_E_streamlit_role TO USER kristoffer_user;
 
 GRANT ROLE ACCOUNTADMIN TO USER rajneet_user;
 
