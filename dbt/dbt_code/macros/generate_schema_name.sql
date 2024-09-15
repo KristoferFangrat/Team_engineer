ec1d2403e4e89a6662912100f9598af432938770
Kristofer Fangrat <kristofer.fangrat@gmail.com>
2024-09-15 20:58:58 +0200

    kommenterade generate_schema_name.sql

--- dbt/dbt_code/macros/generate_schema_name.sql
+++ dbt/dbt_code/macros/generate_schema_name.sql
@@ -1,3 +1,10 @@
+-- Detta makro genererar schemanamn för tabeller och vyer i databasen. Om ett anpassat schemanamn inte anges, 
+-- används standardvärdet från target.schema.
+-- Argument:
+--   custom_schema_name: det anpassade schemanamnet
+--   node: noden som schemat ska genereras för
+-- Returnerar:
+-- (string): Schemanamnet för noden
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
 
