bff9590e316d92e6c1ca48feddb96a7b0f751333
Jasmin <SwedishSolidSnake@gmail.com>
2024-09-16 11:15:09 +0200

    cleaned up roles, revoked dlt from dbt and added specific grants, added better testing, formatting the syntax, etc

--- snowflake_worksheets/check_loaded_data.sql
+++ snowflake_worksheets/check_loaded_data.sql
@@ -1,20 +1,15 @@
 USE ROLE T_E_dlt_role;
-
 USE DATABASE ENGINEER_ADS;
 USE WAREHOUSE DEV_WH;
 USE SCHEMA STAGING;
 
 
 SELECT * FROM engineer_field_job_ads LIMIT 5;
-
 SELECT COUNT(*) FROM engineer_field_job_ads;
-
 SELECT DESCRIPTION__TEXT FROM engineer_field_job_ads LIMIT 10;
-
 SELECT OCCUPATION_GROUP__LABEL FROM engineer_field_job_ads LIMIT 10;
 
-SELECT  DISTINCT(_dlt_load_id) FROM engineer_field_job_ads
-ORDER BY _dlt_load_id;
+
 SELECT  DISTINCT(_dlt_load_id), OCCUPATION__LABEL FROM engineer_field_job_ads
 ORDER BY _dlt_load_id;
 
--- snowflake_worksheets/setup_streamlit_role.sql
+++ snowflake_worksheets/setup_streamlit_role.sql
@@ -1,25 +1,31 @@
-
- USE ROLE useradmin;
-
+--- #### STREAMLIT ROLE
+USE ROLE USERADMIN;
 CREATE ROLE T_E_streamlit_role;
-
 USE ROLE SECURITYADMIN;
 
+--- #### MARTS LAYER GRANTS
 GRANT USAGE ON WAREHOUSE DEV_WH TO ROLE T_E_streamlit_role;
 GRANT USAGE ON DATABASE ENGINEER_ADS TO ROLE T_E_streamlit_role;
 GRANT USAGE ON SCHEMA engineer_ads.marts TO ROLE T_E_streamlit_role;
-
 GRANT SELECT ON ALL TABLES IN SCHEMA ENGINEER_ADS.MARTS TO ROLE T_E_streamlit_ROLE;
 GRANT SELECT ON ALL views IN SCHEMA ENGINEER_ADS.MARTS TO ROLE T_E_streamlit_ROLE;
 GRANT SELECT ON FUTURE TABLES IN SCHEMA ENGINEER_ADS.MARTS TO ROLE T_E_streamlit_ROLE;
 GRANT SELECT ON FUTURE VIEWS IN SCHEMA ENGINEER_ADS.MARTS TO ROLE T_E_streamlit_ROLE;
 
-GRANT ROLE T_E_streamlit_ROLE to user T_E_streamlit;
+--- #### TESTING
+USE ROLE T_E_STREAMLIT_ROLE;
+USE WAREHOUSE DEV_WH;
+USE DATABASE ENGINEER_ADS;
+SHOW TABLES IN Marts;
+SHOW VIEWS IN Marts;
+
+--- #### GRANTING ROLES
 GRANT ROLE T_E_streamlit_role TO USER ricky_user;
 GRANT ROLE T_E_streamlit_role TO USER rajneet_user;
 GRANT ROLE T_E_streamlit_role TO USER kristoffer_user;
-
 GRANT ROLE T_E_streamlit_role TO USER jassef;
+GRANT ROLE T_E_streamlit_role to user T_E_streamlit;
 
-show grants to role t_e_streamlit_role;
---show future grants to role t_e_streamlit_role;
+--- #### SHOW GRANTS
+SHOW GRANTS TO ROLE t_e_streamlit_role;
+SHOW GRANTS ON SCHEMA ENGINEER_ADS.MARTS;
